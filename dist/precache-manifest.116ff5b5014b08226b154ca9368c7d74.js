7731790c62adfe592a9aaa7bbeb19b4a40d05c97
Jake Walker <jakewalk93@hotmail.com>
2020-11-10 21:44:38 +0000

    initial commit

---
+++ dist/precache-manifest.116ff5b5014b08226b154ca9368c7d74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b45a9b8067765bc1ff46",
+    "url": "/css/app.0d7975bf.css"
+  },
+  {
+    "revision": "b45a9b8067765bc1ff46",
+    "url": "/js/app.33853a9d.js"
+  },
+  {
+    "revision": "15a64d58604c4e1c002b",
+    "url": "/js/chunk-vendors.2bd72331.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3522d4e873fd9d6c26fdfe66a84e6dbb",
+    "url": "/index.html"
+  }
+];
