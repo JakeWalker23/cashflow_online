639de96fc2cdb57ba2c64e4e5396992411192ddc
Jake Walker <jakewalk93@hotmail.com>
2020-11-29 21:49:09 +0000

    Dashboard will pull through all the financial data and display it meaningfully.

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>Welcome to Cashflow online. Sign up for free here.</h1>
+    <upload></upload>
+  </div>
+</template>
+
+<script>
+import Upload from '@/components/Upload.vue'
+
+export default {
+  name: 'dashboard',
+  components: {
+    Upload
+  }
+}
+</script>
