b1baf00c26e46d8ee91f25f958053d81a37b03c8
Jake Walker <jakewalk93@hotmail.com>
2021-02-16 21:26:04 +0000

    Transaction component adde

---
+++ src/components/Transaction.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="transaction">
+        <div class="transaction__description">
+            <p>{{description}}</p>
+        </div>
+        <div class="transaction__data">
+            <h5>Amount:</h5>            
+            <p>£{{amount}}</p>
+            <h5>Type:</h5>            
+            <p>{{type}}</p>
+            <h5>Date:</h5>
+            <p>{{date}}</p>
+        </div>   
+    </div>
+</template>
+
+<script>
+export default {
+  name: "transaction",
+  props: ["type", "description", "date", "amount"],
+};
+</script>
+
+<style lang="scss" scoped>
+.transaction {
+  background: white;
+  height: 175px;
+  width: 100%;
+  color: white;
+  font-size: 16px;
+  border: 2px solid #42b983;
+  margin-bottom: 50px;
+
+  @media (min-width: 300px) {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+
+  &__description {
+    font-size: 32px;
+    color: #42b983
+  }
+
+  &__data {
+    display: flex;
+    justify-content: space-between;
+    font-size: 32px;
+    padding-top: 0px;
+    margin: 0px 20px 0px 20px;
+    color: #42b983
+  }
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,8 +7,9 @@
       <option v-for="transaction in transactions" v-bind:value="transaction.value">{{transaction.type}}</option>
     </select>
     <button @click="getCashflow()">Search</button>
-    <h5>{{this.finances}}</h5>
-    <Upload/>
+    <div class="transaction">
+    <Transaction v-for="data in this.finances" v-bind:value="data" v-bind:type="data.transaction_type" v-bind:description="data.transaction_description" v-bind:date="data.transaction_date" v-bind:amount="data.debited_amount"/>
+    </div>
     <Footer/>
   </div>
 </template>
@@ -16,13 +17,14 @@
 <script>
 import Upload from "@/components/Upload.vue";
 import Footer from "@/components/Footer.vue";
-import axios from 'axios';
+import Transaction from "@/components/Transaction.vue";
+import axios from "axios";
 
 export default {
   name: "dashboard",
   components: {
-    Upload,
     Footer,
+    Transaction,
   },
   data() {
     return {
@@ -50,10 +52,12 @@ export default {
   },
   methods: {
     async getCashflow() {
-      this.finances = []
+      this.finances = [];
 
-      let herokuURL = `https://cashflow-onlinee-api.herokuapp.com/api/cashflow/${this.selected}`;
-      
+      let herokuURL = `https://cashflow-onlinee-api.herokuapp.com/api/cashflow/${
+        this.selected
+      }`;
+
       axios.get(herokuURL).then(response => {
         response.data.forEach(statement => {
           this.finances.push(statement);
@@ -64,10 +68,16 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 * {
   margin: 0px;
 }
+
+
+.transaction {
+  margin-top: 10px;
+}
+
 
 .welcome {
   margin-bottom: 16px;
