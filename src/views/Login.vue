2989eeda4d22777e18faefbb432e139d9cab2621
Jake Walker <jakewalk93@hotmail.com>
2021-04-10 16:52:30 +0100

    Initial pushes for login page

---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+  <h1 class="header">Cashflow</h1>
+  <h4>Cashflow helps control your finances</h4>  
+  <Login/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  
+}
+
+.header {
+  color: #42b983;
+  font-size: 75px;
+}
+
+</style>
+
+
+<script>
+import Login from "@/components/Login.vue";
+
+export default {
+    name: "login",
+    components: {
+        Login
+    }
+}
+</script>
+
