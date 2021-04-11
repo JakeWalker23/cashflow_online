0968ed1fa16308b0662fbd78710e27de1080f07c
Jake Walker <jakewalk93@hotmail.com>
2021-04-11 09:08:14 +0100

    Added Desktop responsiveness

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,41 @@
 <template>
   <div class="container">
-    <div>
-        <div class="login">
-        <h1 class="login__header">Cashflow</h1>
+    <div class="items">
+      <div class="login">
+        <h1 class="header">Cashflow</h1>
         <h4>Cashflow helps you control your finances</h4>  
-        </div>
-        <Login/>
+      </div>
+      <Login/>
     </div>
   </div>  
 </template>
 
 <style lang="scss" scoped>
 
-
-.login {
+.items {
   border: 1px solid red;
-  @media(max-width: 500px) {
+  max-width: 1200px;
+
+   @media(min-width: 500px) {
       border: 1px solid red;
       display: flex;
+      justify-content: center;
+      margin-top: 150px;
+}
+} 
+
+
+.login {
+  border: 1px solid white;
+
+  @media (min-width: 500px) {
+    margin-right: 100px;
+  }
   }
 
-  &__header {
+.header {
     color: #42b983;
     font-size: 50px;
-  }
 }
 
 </style>
